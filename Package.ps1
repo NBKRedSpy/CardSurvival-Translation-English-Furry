ee4c51fd5de538f00da9eae0bfe2dd25d70aff73
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-04-09 16:18:59 -0400

    Package and cleanup

---
+++ Package.ps1
@@ -0,0 +1,15 @@
+# Creates the release's .zip file
+
+$ModName = "Guil-天降福瑞";
+$ModFolder = "./Package/" + $ModName
+$ArchiveName = "Guil-Furry.zip"
+
+Remove-Item -ErrorAction SilentlyContinue -Recurse ./Package/*
+Remove-Item -ErrorAction SilentlyContinue $ArchiveName
+mkdir -ErrorAction SilentlyContinue $ModFolder
+
+Copy-Item -Recurse ./Guil-天降福瑞/* $ModFolder
+
+# English name since github strips Unicode for security purposes.
+Compress-Archive -DestinationPath $ArchiveName -Path ./Package/*
+
